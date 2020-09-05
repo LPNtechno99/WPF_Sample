854025ec2d37854ab2165ef3c226b7ce3397847a
LPNtechno99 <lpntechno99@gmail.com>
2020-09-05 17:40:33 +0700

    cap nhat luc 5:40 05/09/2020

---
+++ LiveChartSample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LiveChartSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnMaterial_Click(object sender, RoutedEventArgs e)
+        {
+            Window wd = new Window
+            {
+                Title = "UC material",
+                Content = new ucChart_MaterialCards()
+            };
+            wd.ShowDialog();
+        }
+    }
+}
